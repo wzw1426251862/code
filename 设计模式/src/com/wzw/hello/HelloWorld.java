5b6259914cc742d35169f1b0e8348887650de7b7
wengzhengwei <1347851812@qq.com>
2021-07-11 18:26:16 +0800

    2021-07-11-wzw:helloworld提交

---
+++ 设计模式/src/com/wzw/hello/HelloWorld.java
@@ -0,0 +1,20 @@
+package com.wzw.hello;
+
+/**
+ * @author wengzhengwei
+ * @ClassName HelloWorld.java
+ * @Description helloworld
+ * @createTime 2021年07月11日 18:24:00
+ */
+public class HelloWorld {
+    /**
+     * @Description:主方法
+     * @Param:
+     * @return:
+     * @Author: wengzhengwei
+     * @date: 2021/7/11
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
